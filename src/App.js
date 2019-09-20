180a6e221617ee540f7c55147cecb5a641c10c61
Maxwell Beard <maxwelljamesbeard@gmail.com>
2019-09-20 08:16:23 +0100

    set up, axios requests, useEffect and useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./components/Header.js";
+import axios from 'axios';
 
+const rickAndMortyAPI = `https://rickandmortyapi.com/api/character/`
 
 export default function App() {
+
+  const[char, setChar] = useState([]);
+
+  useEffect(() => {
+    axios.get(rickAndMortyAPI)
+    .then(response => {
+      console.log(response.data.results);
+      setChar=(response.data.results);
+    })
+    .catch(error =>{
+      console.log("this is an error", error);
+    })
+
+  },[])
+  
+
   return (
     <main>
       <Header />
+      {/* <CharacterList char={char} /> */}
     </main>
   );
 }
