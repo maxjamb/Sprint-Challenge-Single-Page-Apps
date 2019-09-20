605a53b1debab0ab46a91023909e7f2525408245
Maxwell Beard <maxwelljamesbeard@gmail.com>
2019-09-20 08:55:17 +0100

    switched useEffect/State, mapped characterList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import Header from "./components/Header.js";
-import axios from 'axios';
 
-const rickAndMortyAPI = `https://rickandmortyapi.com/api/character/`
 
 export default function App() {
-
-  const[char, setChar] = useState([]);
-
-  useEffect(() => {
-    axios.get(rickAndMortyAPI)
-    .then(response => {
-      console.log(response.data.results);
-      setChar=(response.data.results);
-    })
-    .catch(error =>{
-      console.log("this is an error", error);
-    })
-
-  },[])
-  
 
   return (
     <main>
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
+  const[char, setChar] = useState([]);
+
   useEffect(() => {
+    const rickAndMortyAPI = `https://rickandmortyapi.com/api/character/`
+    axios.get(rickAndMortyAPI)
+    .then(response => {
+      console.log(response.data.results);
+      setChar=(response.data.results);
+    })
+    .catch(error =>{
+      console.log("this is an error", error);
+    })
+
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>{characters.map((character => {
+        return (
+          <CharacterCard key={character.id} characters={character} />
+        )
+      }))}</h2>
     </section>
   );
 }
