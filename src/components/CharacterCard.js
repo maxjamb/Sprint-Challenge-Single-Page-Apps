1710b7f9ec12baf84fe701a639c358f1ffbda403
Maxwell Beard <maxwelljamesbeard@gmail.com>
2019-11-15 09:44:57 +0000

    character component running with basic card styles

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+const CharacterCardStyles = styled.div`
+  border: 1px solid grey;
+  margin: 0 auto;
+  width: 400px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: silver;
+  margin-bottom: 30px;
+  border-radius: 1rem;
+
+  img {
+    width: 100%;
+  }
+`;
+
+const CharacterCard = props => {
+  console.log(props);
+  const { name, image, species, status, gender } = props.char;
+  return (
+    <CharacterCardStyles>
+      <h1>{name}</h1>
+      <img src={image} />
+      <p>Species: {species}</p>
+      <p>Status: {status}</p>
+      <p>Gender: {gender}</p>
+    </CharacterCardStyles>
+  );
+};
+
+export default CharacterCard;
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,36 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
-export default function CharacterList() {
+const CharacterList = () => {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
+    axios
+      .get("https://rick-api.herokuapp.com/api/character/")
+      .then(response => {
+        console.log(response);
+        setCharacters(response.data.results);
+        console.log("characters", characters);
+      })
+      .catch(error => {
+        console.log("this is an error", error);
+      });
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters
+        ? characters.map((char, index) => (
+            <CharacterCard key={index} char={char} />
+          ))
+        : null}
     </section>
   );
-}
+};
+
+export default CharacterList;
